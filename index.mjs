7771f2f705745f571d775470f8adae5e1dcd0fb1
thryec <thryec@gmail.com>
2022-05-03 22:34:45 +0800

    hide alice's move then reveal

--- index.mjs
+++ index.mjs
@@ -18,6 +18,7 @@ const HAND = ['Rock', 'Paper', 'Scissors']
 const OUTCOME = ['Bob wins', 'Draw', 'Alice wins']
 
 const Player = (Who) => ({
+  ...stdlib.hasRandom,
   getHand: () => {
     const hand = Math.floor(Math.random() * 3)
     console.log(`${Who} played ${HAND[hand]}`)
